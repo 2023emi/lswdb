600d4563fe354d66aff7bf1f95288d5f8cd33856
2023emi <142286257+2023emi@users.noreply.github.com>
2023-10-31 18:42:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -160,11 +160,13 @@ export PATH=$PATH:~/.fzf/bin
 #
 fsl() {
     local selected_file
-    selected_file=$(find ~/. -type f -name '*.txt' -exec cat {} \; | fzf --preview 'echo {}' --preview-window=up:3:wrap --prompt 'Select file to edit: ')
+    selected_file=$(find /home/. -type f -name '*.xtxt' -exec cat {} \; | fzf --preview 'echo {}' --preview-window=up:3:wrap --prompt 'Select file to edit: ')
     if [[ -n "$selected_file" ]]; then
         echo -n "$selected_file" | xclip -selection clipboard
     fi
 }
+
+
 #
 
 EOL
