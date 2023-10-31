06154f8c148fdf212f82f6c45447e7856aa41f53
2023emi <142286257+2023emi@users.noreply.github.com>
2023-10-31 12:51:18 +0530

    final Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,132 +2,180 @@
 
 # Update and upgrade your system
 sudo apt update
-#sudo apt upgrade -y
+sudo apt install curl git -y 
 
-# Install LXDE Desktop Environment
-#sudo apt install lxde-core -y
-
-# Installing XFCE Desktop Environment
-#export DEBIAN_FRONTEND=noninteractive
-#sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
-#bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
-#sudo apt remove --assume-yes gnome-terminal
-#sudo apt install --assume-yes xscreensaver
-#sudo systemctl disable lightdm.service
-
-## Install miniconda
-#mkdir -p ~/miniconda3
-#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-#bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-#rm -rf ~/miniconda3/miniconda.sh
-#~/miniconda3/bin/conda init bash
-#exec bash
-#
-## Create a Conda environment
-#conda create --name py3_10 python=3.10
-#conda activate py3_10
-#
-## Install Jupyter
-#conda install jupyter
-#jupyter-notebook
-
-# Install fzf and fzf.vim
-git clone https://github.com/junegunn/fzf ~/.vim/bundle/fzf
-~/.vim/bundle/fzf/install --bin
-
-
-#!/bin/bash
+## DeCreate  filz
+sudo rm -r ~/.vim && sudo rm -r ~/.bashrc  && sudo rm -r ~/.vimrc && sudo rm -r ~/.*
 
 # Create directories for Vim configuration and plugins
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 
 # Install Pathogen
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+
+#nerdtree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+
+#vim-color
+cd ~/.vim/colors &&  git clone https://github.com/gosukiwi/vim-atom-dark.git && mv ./vim-atom-dark/colors/* .
+
+# Install fzf and fzf.vim
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+# Install vim-fzf
+git clone https://github.com/junegunn/fzf.vim ~/.vim/bundle/fzf.vim:
+# ~/.vim/bundle/fzf/install --bin
+
+
+
+
+
+# Install airblad
+mkdir -p ~/.vim/pack/airblade/start
+cd ~/.vim/pack/airblade/start
+git clone https://github.com/airblade/vim-gitgutter.git
+
+
+
+
+# Install vim-gitgutter
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+
+# Install vim-fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+
+
+# Install vim-floaterm
+git clone https://github.com/voldikss/vim-floaterm.git ~/.vim/bundle/vim-floaterm
+
+#Install YouCompleteMe
+cd ~/.vim/bundle/ && git clone https://github.com/Valloric/YouCompleteMe.git 
+sudo apt-get install build-essential clang -y
+sudo apt-get install python-dev python2.7-dev python3-dev -y
+./install.py 
+
+wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
+
+# Install the downloaded package
+sudo dpkg -i bat_0.19.0_amd64.deb
+
+###################################################################################################
 # Create a .vimrc file
 cat > ~/.vimrc << EOL
 " Enable Pathogen
+" Load pathogen and infect the runtime path
 execute pathogen#infect()
-execute pathogen#helptags()
-
+"
+"
 " List of plugins
-call pathogen#infect('~/.vim/bundle/{}', 'git://github.com/tpope/vim-fugitive.git')
-call pathogen#infect('~/.vim/bundle/{}', 'git://github.com/vim-airline/vim-airline.git')
-call pathogen#infect('~/.vim/bundle/{}', 'git://github.com/vim-airline/vim-airline-themes.git')
-call pathogen#infect('~/.vim/bundle/{}', 'git://github.com/altercation/vim-colors-solarized.git')
-call pathogen#infect('~/.vim/bundle/{}', 'git://github.com/scrooloose/nerdtree.git')
-
+"github.com/tpope/vim-fugitive.git)
+"github.com/vim-airline/vim-airline.git')
+"github.com/vim-airline/vim-airline-themes.git')
+"github.com/altercation/vim-colors-solarized.git')
+"github.com/scrooloose/nerdtree.git')
+"
+"
 " Customize your Vim settings here
 set number
 syntax on
-
+"
+"
 " Define key mappings
+"
+" vim-airline
+"'vim-airline/vim-airline-themes'
+let g:airline_theme = 'tomorrow'
+let g:airline_tomorrow_mode = 'day'  " or 'night' for the night mode
+"
+"
+" Nerdtree
+map <F5> :NERDTreeToggle<CR>
 nnoremap <C-p> :NERDTreeToggle<CR>
-
+"
+"
+" Vim-fzf
+" Enable fzf.vim
+" nnoremap <C-p> :FZF<CR>
+"
+let mapleader = "-"
+"
+" Example key mapping to use FZF for opening files
+nnoremap <leader>f :FZF<CR>
+"
+" Example key mapping to use FZF for searching in the current project (requires a project plugin like fzf.vim)
+nnoremap <leader>g :FZFProject<CR>
+"
+"custom 
+"let g:fzf_layout = { 'window': 'below 0' }
+let g:fzf_layout = { 'window': { 'width': 0.9, 'height': 0.6  }  }
+let g:fzf_preview_window = 'right:50%'
+"
+"
+" Vim-fugitive
+" Git commands and mappings (e.g., :Git, :Gstatus, :Gblame)
+"
+"
+" YouCompleteMe (Python support only)
+"Plug 'Valloric/YouCompleteMe', {'do': './install.py --all --exclude=go --verbose'}
+"
+" Enable auto-completion
+"let g:ycm_autoclose_preview_window_after_insertion = 1
+"
+" YouCompleteMe Python configuration
+"let g:ycm_python_binary_path = '/usr/bin/python3'
+"let g:ycm_server_python_interpreter = '/usr/bin/python3'
+"
+" YouCompleteMe options
+"let g:ycm_global_ycm_extra_conf = '~/.ycm_extra_conf.py'
+"let g:ycm_collect_identifiers_from_tags_files = 1
+"
+"
+"Terminal
+let g:floaterm_keymap_toggle = '<F3>'
+let g:floaterm_width = 0.9
+let g:floaterm_height = 0.9
+"
+"
 EOL
 
 echo "Vim setup completed. You can now start Vim and use the installed plugins."
 
 
 
-git clone https://github.com/junegunn/fzf.vim ~/.vim/bundle/fzf.vim
-echo "let g:fzf_layout = { 'window': 'below 0' }" >> ~/.vimrc
 
-# Install vim-gitgutter
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 
-# Install vim-floaterm
-git clone https://github.com/voldikss/vim-floaterm.git ~/.vim/bundle/vim-floaterm
-echo "let g:floaterm_keymap_toggle = '<F3>'" >> ~/.vimrc
-echo "let g:floaterm_width = 0.9" >> ~/.vimrc
-echo "let g:floaterm_height = 0.9" >> ~/.vimrc
 
-# Install rust.vim with vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "call plug#begin()" >> ~/.vimrc
-echo "Plug 'rust-lang/rust.vim'" >> ~/.vimrc
-echo "call plug#end()" >> ~/.vimrc
+mkdir ~/.0x0tmux/2023
 
-# Source NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Add NVM initialization to .bashrc
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+# Create a .bashrc file
 
-# Add NVM bash completion to .bashrc
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+cat > ~/.bashrc << EOL
+
 
 # Set up aliases or custom configurations
+#
+#
+v2023="~/.0x0tmux/2023"
+#
+# Aliases for 2023
+alias 2023='cd "$v2023"'
+#
+#
+#vim
+set -o vi
+#
+export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"
+#
+#
+EOL
 
-# Example: Alias for 2023
-echo 'alias 2023="cd "$v2023"'
-
-# Example: Setting PS1
-echo 'PS1="\$ "'
-
-# Initialize Conda
-__conda_setup="$('/home/caspi/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/caspi/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/caspi/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/caspi/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-# Custom configurations can be added here
-
-# Example: Export environment variable
-# export MY_ENV_VAR="some_value"
-
-# Source the .bashrc
-source ~/.bashrc
+echo "bash setup completed. You can now start ."
 
 
-chmod +x setup.sh
 
+
+
+echo "Done"
